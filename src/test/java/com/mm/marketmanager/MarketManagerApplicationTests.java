ab3f951d1f34d0e8fb5cf8d04088ebbfb39fdd9f
caito <caitocd@gmail.com>
2025-11-14 13:09:38 -0300

    first commit

---
+++ src/test/java/com/mm/marketmanager/MarketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.marketmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
